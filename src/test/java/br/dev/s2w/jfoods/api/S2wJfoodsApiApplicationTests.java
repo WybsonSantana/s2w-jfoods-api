dfd32327e32a5d9fa03bd33956f0fd106d4d46a6
WybsonSantana <wybson@hotmail.com>
2022-12-22 22:07:13 -0300

    jFoods - 02.05 - Criando um projeto Spring Boot com Spring Initializr

---
+++ src/test/java/br/dev/s2w/jfoods/api/S2wJfoodsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.s2w.jfoods.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2wJfoodsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
