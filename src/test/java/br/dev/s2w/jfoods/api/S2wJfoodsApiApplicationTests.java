809b5a43810b229936b09c889543458932598588
WybsonSantana <wybson@hotmail.com>
2022-12-30 16:11:53 -0300

    jFoods - 02.24 - Substituindo propriedades via linha de comando e variáveis de ambiente

--- src/test/java/br/dev/s2w/jfoods/api/S2wJfoodsApiApplicationTests.java
+++ src/test/java/br/dev/s2w/jfoods/api/S2wJfoodsApiApplicationTests.java
@@ -1,13 +1,16 @@
 package br.dev.s2w.jfoods.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class S2wJfoodsApiApplicationTests {
+public class S2wJfoodsApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
