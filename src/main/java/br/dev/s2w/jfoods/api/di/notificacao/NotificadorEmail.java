058576419d357719cf35f526b40fe87de18acf25
WybsonSantana <wybson@hotmail.com>
2022-12-30 17:03:20 -0300

    jFoods - 02.25 - Criando e acessando propriedades customizadas com @Value

--- src/main/java/br/dev/s2w/jfoods/api/di/notificacao/NotificadorEmail.java
+++ src/main/java/br/dev/s2w/jfoods/api/di/notificacao/NotificadorEmail.java
@@ -1,15 +1,24 @@
 package br.dev.s2w.jfoods.api.di.notificacao;
 
 import br.dev.s2w.jfoods.api.di.modelo.Cliente;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host}")
+    private String host;
+
+    @Value("${notificador.email.port}")
+    private Integer port;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + host);
+        System.out.println("Port: " + port);
+
         System.out.printf("Notificando %s através do email %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
