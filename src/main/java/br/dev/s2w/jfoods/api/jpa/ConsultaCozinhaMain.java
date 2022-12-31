d035b9a42cdcd9fba6c05f2bccba0d786c0e9bba
WybsonSantana <wybson@hotmail.com>
2022-12-31 13:27:53 -0300

    jFoods - 03.08 - Consultando objetos do banco de dados

---
+++ src/main/java/br/dev/s2w/jfoods/api/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,25 @@
+package br.dev.s2w.jfoods.api.jpa;
+
+import br.dev.s2w.jfoods.api.S2wJfoodsApiApplication;
+import br.dev.s2w.jfoods.api.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class ConsultaCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(S2wJfoodsApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+        for (Cozinha cozinha : cozinhas) {
+            System.out.println(cozinha.getNome());
+        }
+    }
+}
