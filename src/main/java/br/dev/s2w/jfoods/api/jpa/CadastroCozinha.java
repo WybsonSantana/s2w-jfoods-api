7945adb8b19992d12e05bb24526b6b63e23f35e9
WybsonSantana <wybson@hotmail.com>
2022-12-31 19:15:13 -0300

    jFoods - 03.12 - Excluindo um objeto do banco de dados

--- src/main/java/br/dev/s2w/jfoods/api/jpa/CadastroCozinha.java
+++ src/main/java/br/dev/s2w/jfoods/api/jpa/CadastroCozinha.java
@@ -27,4 +27,10 @@ public class CadastroCozinha {
         return manager.find(Cozinha.class, id);
     }
 
+    @Transactional
+    public void remover(Cozinha cozinha) {
+        cozinha = buscar(cozinha.getId());
+        manager.remove(cozinha);
+    }
+
 }
---
+++ src/main/java/br/dev/s2w/jfoods/api/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,22 @@
+package br.dev.s2w.jfoods.api.jpa;
+
+import br.dev.s2w.jfoods.api.S2wJfoodsApiApplication;
+import br.dev.s2w.jfoods.api.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class ExclusaoCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(S2wJfoodsApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+
+        cadastroCozinha.remover(cozinha);
+    }
+}
