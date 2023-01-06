efe6943ec146a644b7a783fbfc07bc5da7c144fb
WybsonSantana <wybson@hotmail.com>
2023-01-06 01:34:36 -0300

    jFoods - 04.13 - Implementando content negotiation para retornar JSON ou XML

--- src/main/java/br/dev/s2w/jfoods/api/gateway/controller/CozinhaController.java
+++ src/main/java/br/dev/s2w/jfoods/api/gateway/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package br.dev.s2w.jfoods.api.gateway.controller;
 import br.dev.s2w.jfoods.api.domain.model.Cozinha;
 import br.dev.s2w.jfoods.api.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas") //, produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
-    public List<Cozinha> listar() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listarJson() {
+        System.out.println("listarJson()");
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+public List<Cozinha> listarXml() {
+        System.out.println("listarXml()");
         return cozinhaRepository.listar();
     }
 
