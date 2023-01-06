c15b0612fb0743beba9687ed1c8d16d95aa58f35
WybsonSantana <wybson@hotmail.com>
2023-01-06 00:36:35 -0300

    jFoods - 04.11 - Desafio - collection resource de estados

---
+++ src/main/java/br/dev/s2w/jfoods/api/gateway/controller/EstadoController.java
@@ -0,0 +1,24 @@
+package br.dev.s2w.jfoods.api.gateway.controller;
+
+import br.dev.s2w.jfoods.api.domain.model.Estado;
+import br.dev.s2w.jfoods.api.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @GetMapping
+    public List<Estado> listar() {
+        return estadoRepository.listar();
+    }
+
+}
