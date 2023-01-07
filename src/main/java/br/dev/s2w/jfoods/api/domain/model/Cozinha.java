234da74b1012b9e24260bb554832a5711a8fc130
WybsonSantana <wybson@hotmail.com>
2023-01-07 13:25:07 -0300

    jFoods - 04.15 - Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName

--- src/main/java/br/dev/s2w/jfoods/api/domain/model/Cozinha.java
+++ src/main/java/br/dev/s2w/jfoods/api/domain/model/Cozinha.java
@@ -1,10 +1,15 @@
 package br.dev.s2w.jfoods.api.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+@JsonRootName("Gastronomia")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -15,6 +20,8 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    //@JsonIgnore
+    @JsonProperty(value = "titulo")
     @Column(nullable = false)
     private String nome;
 
