e75030c344bf244ba21fc594f424a42444ac96f9
WybsonSantana <wybson@hotmail.com>
2023-01-07 16:54:38 -0300

    jFoods - 04.19 - Definindo o status da resposta HTTP com @ResponseStatus

--- src/main/java/br/dev/s2w/jfoods/api/gateway/controller/CozinhaController.java
+++ src/main/java/br/dev/s2w/jfoods/api/gateway/controller/CozinhaController.java
@@ -4,11 +4,9 @@ import br.dev.s2w.jfoods.api.domain.model.Cozinha;
 import br.dev.s2w.jfoods.api.domain.repository.CozinhaRepository;
 import br.dev.s2w.jfoods.api.gateway.model.CozinhasXmlWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +27,7 @@ public class CozinhaController {
         return new CozinhasXmlWrapper(cozinhaRepository.listar());
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{cozinhaId}")
     public Cozinha buscar(@PathVariable Long cozinhaId) {
         return cozinhaRepository.buscar(cozinhaId);
