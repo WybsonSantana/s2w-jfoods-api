38bdb1d14b65783ee697ec7999730f8ef31bdffb
WybsonSantana <wybson@hotmail.com>
2023-01-07 16:10:46 -0300

    jFoods - 04.16 - Customizando a representação em XML com Wrapper e anotações do Jackson

--- src/main/java/br/dev/s2w/jfoods/api/domain/model/Cozinha.java
+++ src/main/java/br/dev/s2w/jfoods/api/domain/model/Cozinha.java
@@ -1,15 +1,12 @@
 package br.dev.s2w.jfoods.api.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
-@JsonRootName("Gastronomia")
+@JsonRootName("cozinha")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -20,8 +17,6 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    //@JsonIgnore
-    @JsonProperty(value = "titulo")
     @Column(nullable = false)
     private String nome;
 
--- src/main/java/br/dev/s2w/jfoods/api/gateway/controller/CozinhaController.java
+++ src/main/java/br/dev/s2w/jfoods/api/gateway/controller/CozinhaController.java
@@ -2,7 +2,9 @@ package br.dev.s2w.jfoods.api.gateway.controller;
 
 import br.dev.s2w.jfoods.api.domain.model.Cozinha;
 import br.dev.s2w.jfoods.api.domain.repository.CozinhaRepository;
+import br.dev.s2w.jfoods.api.gateway.model.CozinhasXmlWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,11 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listarJson() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml() {
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
     }
 
     @GetMapping("/{cozinhaId}")
