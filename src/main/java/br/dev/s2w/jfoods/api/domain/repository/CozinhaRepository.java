e6377154a9a995529aa476834183f9ec2ff29d47
WybsonSantana <wybson@hotmail.com>
2023-02-10 22:55:06 -0300

    jFoods - 05.01 - Implementando consultas JPQL em repositórios

--- src/main/java/br/dev/s2w/jfoods/api/domain/repository/CozinhaRepository.java
+++ src/main/java/br/dev/s2w/jfoods/api/domain/repository/CozinhaRepository.java
@@ -1,12 +1,16 @@
 package br.dev.s2w.jfoods.api.domain.repository;
 
 import br.dev.s2w.jfoods.api.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface CozinhaRepository {
 
     List<Cozinha> listar();
+
+    List<Cozinha> consultarPorNome(String nome);
 
     Cozinha buscar(Long cozinhaId);
 
--- src/main/java/br/dev/s2w/jfoods/api/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/br/dev/s2w/jfoods/api/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -3,14 +3,14 @@ package br.dev.s2w.jfoods.api.infrastructure.repository;
 import br.dev.s2w.jfoods.api.domain.model.Cozinha;
 import br.dev.s2w.jfoods.api.domain.repository.CozinhaRepository;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
     @PersistenceContext
@@ -19,6 +19,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
     @Override
     public List<Cozinha> listar() {
         return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+    }
+
+    @Override
+    public List<Cozinha> consultarPorNome(String nome) {
+        return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+                .setParameter("nome", "%" + nome + "%")
+                .getResultList();
     }
 
     @Override
