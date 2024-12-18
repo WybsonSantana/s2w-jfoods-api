fb02f58a82445a264858ca47f4fcb5e6673569ac
WybsonSantana <wybson@hotmail.com>
2024-12-17 21:35:18 -0300

    JFoods: 02.14 - Knowing the injection points and the @Autowired annotation

--- src/main/java/br/dev/s2w/jfoods/api/di/notification/EmailNotifier.java
+++ src/main/java/br/dev/s2w/jfoods/api/di/notification/EmailNotifier.java
@@ -1,28 +1,14 @@
 package br.dev.s2w.jfoods.api.di.notification;
 
 import br.dev.s2w.jfoods.api.di.model.Customer;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmailNotifier implements Notifier {
-
-    private boolean uppercase;
-    private String hostSmtpServer;
-
-    public EmailNotifier(String hostSmtpServer) {
-        this.hostSmtpServer = hostSmtpServer;
-        System.out.println("EmailNotifier");
-    }
 
     @Override
     public void notify(Customer customer, String message) {
-        if (this.uppercase) {
-            message = message.toUpperCase();
-        }
-
         System.out.printf("Notifying %s by e-mail %s: %s%n",
                 customer.getName(), customer.getEmail(), message);
-    }
-
-    public void setUppercase(boolean uppercase) {
-        this.uppercase = uppercase;
     }
 }
--- src/main/java/br/dev/s2w/jfoods/api/di/service/CustomerActivationService.java
+++ src/main/java/br/dev/s2w/jfoods/api/di/service/CustomerActivationService.java
@@ -2,19 +2,31 @@ package br.dev.s2w.jfoods.api.di.service;
 
 import br.dev.s2w.jfoods.api.di.model.Customer;
 import br.dev.s2w.jfoods.api.di.notification.Notifier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomerActivationService {
 
+    @Autowired
     private Notifier notifier;
 
-    public CustomerActivationService(Notifier notifier) {
-        this.notifier = notifier;
-
-        System.out.printf("CustomerActivationService: %s%n", notifier);
-    }
+//    @Autowired
+//    public CustomerActivationService(Notifier notifier) {
+//        this.notifier = notifier;
+//    }
+//
+//    public CustomerActivationService(String any) {
+//
+//    }
 
     public void activate(Customer customer) {
         customer.activate();
         notifier.notify(customer, "Your registration in the system is active!");
     }
+
+//    @Autowired
+//    public void setNotifier(Notifier notifier) {
+//        this.notifier = notifier;
+//    }
 }
