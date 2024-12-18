d0541bb017bbc192ad1e0df9d4350596c873dc4b
WybsonSantana <wybson@hotmail.com>
2024-12-17 23:13:15 -0300

    JFoods: 02.16 - Bean Ambiguity and Bean List Injection

--- src/main/java/br/dev/s2w/jfoods/api/di/service/CustomerActivationService.java
+++ src/main/java/br/dev/s2w/jfoods/api/di/service/CustomerActivationService.java
@@ -5,19 +5,19 @@ import br.dev.s2w.jfoods.api.di.notification.Notifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class CustomerActivationService {
 
-    @Autowired(required = false)
-    private Notifier notifier;
+    @Autowired
+    private List<Notifier> notifiers;
 
     public void activate(Customer customer) {
         customer.activate();
 
-        if (notifier != null) {
+        for (Notifier notifier : notifiers) {
             notifier.notify(customer, "Your registration in the system is active!");
-        } else {
-            System.out.println("There is no notifier, but the client has been activated.");
         }
     }
 }
