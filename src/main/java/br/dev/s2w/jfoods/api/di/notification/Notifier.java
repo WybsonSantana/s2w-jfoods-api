bf818036e8c8383792591405b0a252b0c2b00f88
WybsonSantana <wybson@hotmail.com>
2024-12-20 22:39:33 -0300

    JFoods: 03.03 - Adding JPA and configuring the Data Source

--- src/main/java/br/dev/s2w/jfoods/api/di/notification/Notifier.java
+++
@@ -1,7 +0,0 @@
-package br.dev.s2w.jfoods.api.di.notification;
-
-import br.dev.s2w.jfoods.api.di.model.Customer;
-
-public interface Notifier {
-    void notify(Customer customer, String message);
-}
--- src/main/java/br/dev/s2w/jfoods/api/di/notification/UrgencyLevel.java
+++
@@ -1,6 +0,0 @@
-package br.dev.s2w.jfoods.api.di.notification;
-
-public enum UrgencyLevel {
-    URGENT,
-    NO_URGENCY
-}
