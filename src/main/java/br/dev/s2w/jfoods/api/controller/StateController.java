326d622b0ed9864cb53afe11d414759d9fcb681b
WybsonSantana <wybson@hotmail.com>
2024-12-22 19:25:50 -0300

    JFoods: 04.11 - Challenge: resource collection of states

---
+++ src/main/java/br/dev/s2w/jfoods/api/controller/StateController.java
@@ -0,0 +1,23 @@
+package br.dev.s2w.jfoods.api.controller;
+
+import br.dev.s2w.jfoods.api.domain.model.State;
+import br.dev.s2w.jfoods.api.domain.repository.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/states")
+public class StateController {
+
+    @Autowired
+    private StateRepository stateRepository;
+
+    @GetMapping
+    public List<State> list() {
+        return stateRepository.list();
+    }
+}
