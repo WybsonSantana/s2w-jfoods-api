bb9356dbd51f5e49b74d2675eafb6dada77885b8
WybsonSantana <wybson@hotmail.com>
2024-12-22 22:56:01 -0300

    JFoods: 04.13 - Implementing content negotiation to return JSON or XML

--- src/main/java/br/dev/s2w/jfoods/api/controller/CuisineController.java
+++ src/main/java/br/dev/s2w/jfoods/api/controller/CuisineController.java
@@ -3,6 +3,7 @@ package br.dev.s2w.jfoods.api.controller;
 import br.dev.s2w.jfoods.api.domain.model.Cuisine;
 import br.dev.s2w.jfoods.api.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cuisines")
+@RequestMapping(value = "/cuisines") //, produces = MediaType.APPLICATION_JSON_VALUE)
 public class CuisineController {
 
     @Autowired
     private CuisineRepository cuisineRepository;
 
-    @GetMapping
-    public List<Cuisine> list() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cuisine> listJson() {
+        return cuisineRepository.list();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Cuisine> listXml() {
         return cuisineRepository.list();
     }
 }
