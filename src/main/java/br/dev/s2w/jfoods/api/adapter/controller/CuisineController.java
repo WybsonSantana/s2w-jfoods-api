57e4e2c423b24e805089bcc1bed1767fa0b3ff2e
WybsonSantana <wybson@hotmail.com>
2024-12-23 15:52:38 -0300

    JFoods: 04.21 - Correcting HTTP Status for non-existent resource

--- src/main/java/br/dev/s2w/jfoods/api/adapter/controller/CuisineController.java
+++ src/main/java/br/dev/s2w/jfoods/api/adapter/controller/CuisineController.java
@@ -4,8 +4,6 @@ import br.dev.s2w.jfoods.api.adapter.model.CuisinesXmlWrapper;
 import br.dev.s2w.jfoods.api.domain.model.Cuisine;
 import br.dev.s2w.jfoods.api.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,15 +34,11 @@ public class CuisineController {
     public ResponseEntity<Cuisine> search(@PathVariable Long cuisineId) {
         Cuisine cuisine = cuisineRepository.search(cuisineId);
 
-        //return ResponseEntity.status(HttpStatus.OK).body(cuisine);
-        //return ResponseEntity.ok(cuisine);
+        if (cuisine != null) {
+            return ResponseEntity.ok(cuisine);
+        }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://api.jfoods.local:8080/cuisines");
-
-        return ResponseEntity
-                .status(HttpStatus.FOUND)
-                .headers(headers)
-                .build();
+        //return ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+        return ResponseEntity.notFound().build();
     }
 }
