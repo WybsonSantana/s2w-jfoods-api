c710737dec8ea801c057ad927e5493870eb85c92
WybsonSantana <wybson@hotmail.com>
2024-12-23 14:59:19 -0300

    JFoods: 04.20 - Handling HTTP response with ResponseEntity

--- src/main/java/br/dev/s2w/jfoods/api/adapter/controller/CuisineController.java
+++ src/main/java/br/dev/s2w/jfoods/api/adapter/controller/CuisineController.java
@@ -4,9 +4,14 @@ import br.dev.s2w.jfoods.api.adapter.model.CuisinesXmlWrapper;
 import br.dev.s2w.jfoods.api.domain.model.Cuisine;
 import br.dev.s2w.jfoods.api.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -27,9 +32,19 @@ public class CuisineController {
         return new CuisinesXmlWrapper((cuisineRepository.list()));
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{cuisineId}")
-    public Cuisine search(@PathVariable Long cuisineId) {
-        return cuisineRepository.search(cuisineId);
+    public ResponseEntity<Cuisine> search(@PathVariable Long cuisineId) {
+        Cuisine cuisine = cuisineRepository.search(cuisineId);
+
+        //return ResponseEntity.status(HttpStatus.OK).body(cuisine);
+        //return ResponseEntity.ok(cuisine);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, "http://api.jfoods.local:8080/cuisines");
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .headers(headers)
+                .build();
     }
 }
