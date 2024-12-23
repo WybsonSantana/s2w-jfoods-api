43c8f341c707e273e25a3d1144b939964c411429
WybsonSantana <wybson@hotmail.com>
2024-12-22 23:41:08 -0300

    JFoods: 04.14 - Querying Singleton Resource with GET and @PathVariable

--- src/main/java/br/dev/s2w/jfoods/api/controller/CuisineController.java
+++ src/main/java/br/dev/s2w/jfoods/api/controller/CuisineController.java
@@ -3,27 +3,27 @@ package br.dev.s2w.jfoods.api.controller;
 import br.dev.s2w.jfoods.api.domain.model.Cuisine;
 import br.dev.s2w.jfoods.api.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/cuisines") //, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/cuisines")
 public class CuisineController {
 
     @Autowired
     private CuisineRepository cuisineRepository;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Cuisine> listJson() {
+    @GetMapping
+    public List<Cuisine> list() {
         return cuisineRepository.list();
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public List<Cuisine> listXml() {
-        return cuisineRepository.list();
+    @GetMapping("/{cuisineId}")
+    public Cuisine search(@PathVariable Long cuisineId) {
+        return cuisineRepository.search(cuisineId);
     }
 }
