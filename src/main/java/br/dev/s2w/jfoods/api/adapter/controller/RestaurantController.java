cdc6720188fe477258bfded7455fd57ea2213492
WybsonSantana <wybson@hotmail.com>
2024-12-24 15:15:09 -0300

    JFoods: 04.30 - Modeling and implementing restaurant feature inclusion

--- src/main/java/br/dev/s2w/jfoods/api/adapter/controller/RestaurantController.java
+++ src/main/java/br/dev/s2w/jfoods/api/adapter/controller/RestaurantController.java
@@ -1,13 +1,13 @@
 package br.dev.s2w.jfoods.api.adapter.controller;
 
+import br.dev.s2w.jfoods.api.domain.exception.EntityNotFoundException;
 import br.dev.s2w.jfoods.api.domain.model.Restaurant;
 import br.dev.s2w.jfoods.api.domain.repository.RestaurantRepository;
+import br.dev.s2w.jfoods.api.domain.service.RestaurantRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +17,9 @@ public class RestaurantController {
 
     @Autowired
     private RestaurantRepository restaurantRepository;
+
+    @Autowired
+    private RestaurantRegisterService restaurantRegister;
 
     @GetMapping
     public List<Restaurant> list() {
@@ -32,5 +35,16 @@ public class RestaurantController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> add(@RequestBody Restaurant restaurant) {
+        try {
+            restaurant = restaurantRegister.save(restaurant);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(restaurant);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/br/dev/s2w/jfoods/api/domain/service/RestaurantRegisterService.java
@@ -0,0 +1,32 @@
+package br.dev.s2w.jfoods.api.domain.service;
+
+import br.dev.s2w.jfoods.api.domain.exception.EntityNotFoundException;
+import br.dev.s2w.jfoods.api.domain.model.Cuisine;
+import br.dev.s2w.jfoods.api.domain.model.Restaurant;
+import br.dev.s2w.jfoods.api.domain.repository.CuisineRepository;
+import br.dev.s2w.jfoods.api.domain.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestaurantRegisterService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @Autowired
+    private CuisineRepository cuisineRepository;
+
+    public Restaurant save(Restaurant restaurant) {
+        Long cuisineId = restaurant.getCuisine().getId();
+        Cuisine currentCuisine = cuisineRepository.search(cuisineId);
+
+        if (currentCuisine == null) {
+            throw new EntityNotFoundException(String.format("There is no cuisine registration with the code %d", cuisineId));
+        }
+
+        restaurant.setCuisine(currentCuisine);
+
+        return restaurantRepository.save(restaurant);
+    }
+}
