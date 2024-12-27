f78483effad31e00b03bdbe02cab23dbd06df0ed
WybsonSantana <wybson@hotmail.com>
2024-12-27 17:05:46 -0300

    JFoods: 05.06 - Creating queries with query methods

--- src/main/java/br/dev/s2w/jfoods/api/adapter/controller/TestController.java
+++ src/main/java/br/dev/s2w/jfoods/api/adapter/controller/TestController.java
@@ -1,9 +1,15 @@
 package br.dev.s2w.jfoods.api.adapter.controller;
 
+import br.dev.s2w.jfoods.api.domain.model.Cuisine;
 import br.dev.s2w.jfoods.api.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/test")
@@ -12,8 +18,13 @@ public class TestController {
     @Autowired
     private CuisineRepository cuisineRepository;
 
-//    @GetMapping("/cuisines/by-name")
-//    public List<Cuisine> cuisinesByName(@RequestParam("name") String name) {
-//        return cuisineRepository.findByName(name);
-//    }
+    @GetMapping("/cuisines/by-name")
+    public List<Cuisine> cuisinesByName(@RequestParam String name) {
+        return cuisineRepository.findAllByName(name);
+    }
+
+    @GetMapping("/cuisines/unique-by-name")
+    public Optional<Cuisine> cuisineByName(@RequestParam String name) {
+        return cuisineRepository.findByName(name);
+    }
 }
--- src/main/java/br/dev/s2w/jfoods/api/domain/repository/CuisineRepository.java
+++ src/main/java/br/dev/s2w/jfoods/api/domain/repository/CuisineRepository.java
@@ -4,7 +4,12 @@ import br.dev.s2w.jfoods.api.domain.model.Cuisine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CuisineRepository extends JpaRepository<Cuisine, Long> {
-    //List<Cuisine> findByName(String name);
+    List<Cuisine> findAllByName(String name);
+
+    Optional<Cuisine> findByName(String name);
 }
