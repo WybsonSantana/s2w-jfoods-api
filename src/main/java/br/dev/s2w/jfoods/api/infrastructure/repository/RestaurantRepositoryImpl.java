6630e8a92ceddfc6388261273b262fdf936f8569
WybsonSantana <wybson@hotmail.com>
2024-12-29 00:24:02 -0300

    JFoods: 05.13 - Implementing a simple query with Criteria API

--- src/main/java/br/dev/s2w/jfoods/api/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/br/dev/s2w/jfoods/api/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -3,13 +3,13 @@ package br.dev.s2w.jfoods.api.infrastructure.repository;
 import br.dev.s2w.jfoods.api.domain.model.Restaurant;
 import br.dev.s2w.jfoods.api.domain.repository.RestaurantRepositoryQueries;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -20,31 +20,12 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
 
     @Override
     public List<Restaurant> find(String name, BigDecimal initialFee, BigDecimal finalFee) {
-        var jpql = new StringBuilder();
-        var parameters = new HashMap<String, Object>();
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
 
-        jpql.append("from Restaurant where 0 = 0 ");
+        CriteriaQuery<Restaurant> criteria = builder.createQuery(Restaurant.class);
+        criteria.from(Restaurant.class);
 
-        if (StringUtils.hasLength(name)) {
-            jpql.append("and name like :name ");
-            parameters.put("name", "%" + name + "%");
-        }
-
-        if (initialFee != null) {
-            jpql.append("and deliveryFee >= :initialFee ");
-            parameters.put("initialFee", initialFee);
-        }
-
-        if (finalFee != null) {
-            jpql.append("and deliveryFee <= :finalFee ");
-            parameters.put("finalFee", finalFee);
-        }
-
-        TypedQuery<Restaurant> query = manager.createQuery(jpql.toString(), Restaurant.class);
-
-        parameters.forEach((key, value) ->
-                query.setParameter(key, value)
-        );
+        TypedQuery<Restaurant> query = manager.createQuery(criteria);
 
         return query.getResultList();
     }
