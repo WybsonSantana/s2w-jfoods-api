cd29cf0d20cfed8fb87a9d6fc543b9846871bee8
WybsonSantana <wybson@hotmail.com>
2025-01-02 22:30:21 -0300

    JFoods: 06.09 - Challenge: Mapping many-to-many relationships

---
+++ src/main/java/br/dev/s2w/jfoods/api/domain/model/Identity.java
@@ -0,0 +1,39 @@
+package br.dev.s2w.jfoods.api.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Identity {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime registrationDate;
+
+    @ManyToMany
+    @JoinTable(name = "identity_role",
+            joinColumns = @JoinColumn(name = "identity_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles = new ArrayList<>();
+}
---
+++ src/main/java/br/dev/s2w/jfoods/api/domain/model/Role.java
@@ -0,0 +1,27 @@
+package br.dev.s2w.jfoods.api.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Role {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "role_permission",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private List<Permission> permissions = new ArrayList<>();
+}
