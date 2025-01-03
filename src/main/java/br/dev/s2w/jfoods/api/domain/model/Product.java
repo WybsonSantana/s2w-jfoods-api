09a15c92c3d9b8e63bdb89ed6bfaeafbc3bad850
WybsonSantana <wybson@hotmail.com>
2025-01-02 21:40:59 -0300

    JFoods: 06.07 - Challenge: mapping many-to-one relationships

---
+++ src/main/java/br/dev/s2w/jfoods/api/domain/model/Product.java
@@ -0,0 +1,33 @@
+package br.dev.s2w.jfoods.api.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Product {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurant restaurant;
+}
