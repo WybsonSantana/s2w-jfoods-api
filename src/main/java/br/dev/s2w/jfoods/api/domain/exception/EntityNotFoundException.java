18f50879ddb12a5db850ddb13d3b8f7cca0fe9c9
WybsonSantana <wybson@hotmail.com>
2025-01-04 18:15:49 -0300

    JFoods: 08.04 - Extending ResponseStatusException

--- src/main/java/br/dev/s2w/jfoods/api/domain/exception/EntityNotFoundException.java
+++ src/main/java/br/dev/s2w/jfoods/api/domain/exception/EntityNotFoundException.java
@@ -1,14 +1,18 @@
 package br.dev.s2w.jfoods.api.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entity not foud")
-public class EntityNotFoundException extends RuntimeException {
+//@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entity not foud")
+public class EntityNotFoundException extends ResponseStatusException {
 
     private static final long serialVersionUID = 1L;
 
+    public EntityNotFoundException(HttpStatus status, String message) {
+        super(status, message);
+    }
+
     public EntityNotFoundException(String message) {
-        super(message);
+        this(HttpStatus.NOT_FOUND, message);
     }
 }
