690262024d7e8a9e5f03a81166ded723b852f53e
WybsonSantana <wybson@hotmail.com>
2025-02-01 19:49:20 -0300

    JFoods: 10.08 - Implementing API Tests with REST Assured and validating the HTTP status code

--- src/test/java/br/dev/s2w/jfoods/api/CuisineRegisterIT.java
+++ src/test/java/br/dev/s2w/jfoods/api/CuisineRegisterIT.java
@@ -1,60 +1,35 @@
 package br.dev.s2w.jfoods.api;
 
-import br.dev.s2w.jfoods.api.domain.exception.EntityInUseException;
-import br.dev.s2w.jfoods.api.domain.exception.EntityNotFoundException;
-import br.dev.s2w.jfoods.api.domain.model.Cuisine;
-import br.dev.s2w.jfoods.api.domain.service.CuisineRegisterService;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.validation.ConstraintViolationException;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
+import static io.restassured.RestAssured.given;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CuisineRegisterIT {
 
-    @Autowired
-    private CuisineRegisterService cuisineRegister;
+    @LocalServerPort
+    private int port;
 
     @Test
-    public void shouldAssignIdWhenRegisteringCuisineWithCorrectData() {
-        Cuisine newCuisine = new Cuisine();
-        newCuisine.setName("Chinesa");
+    public void shouldReturnStatus200WhenQueryingCuisines() {
+        enableLoggingOfRequestAndResponseIfValidationFails();
 
-        newCuisine = cuisineRegister.save(newCuisine);
-
-        assertThat(newCuisine.getId()).isNotNull();
-    }
-
-    @Test
-    public void shouldFailWhenRegisteringCuisineWithoutName() {
-        Cuisine newCuisine = new Cuisine();
-        newCuisine.setName(null);
-
-        assertThrows(ConstraintViolationException.class, () ->
-                cuisineRegister.save(newCuisine));
-    }
-
-    @Test
-    public void shouldFailWhenDeletingCuisineInUse() {
-        Long cuisineId = 1L;
-
-        assertThrows(EntityInUseException.class, () ->
-                cuisineRegister.remove(cuisineId));
-    }
-
-    @Test
-    public void shouldFailWhenDeletingCuisineNotFound() {
-        Long cuisineId = 100L;
-
-        assertThrows(EntityNotFoundException.class, () ->
-                cuisineRegister.remove(cuisineId));
+        given()
+                .basePath("/cuisines")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(HttpStatus.OK.value());
     }
 
 }
