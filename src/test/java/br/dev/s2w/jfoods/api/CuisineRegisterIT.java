51f5b8c4c207115e6fb93a081ff8f97fd971897d
WybsonSantana <wybson@hotmail.com>
2025-02-02 00:15:57 -0300

    JFoods: 10.13 - Setting up a test bench and using @TestPropertySource

--- src/test/java/br/dev/s2w/jfoods/api/CuisineRegisterIT.java
+++ src/test/java/br/dev/s2w/jfoods/api/CuisineRegisterIT.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.*;
@@ -17,6 +18,7 @@ import static org.hamcrest.Matchers.hasSize;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CuisineRegisterIT {
 
     @LocalServerPort
