699b80109970e8a656661df170727c1df4c0bdb3
WybsonSantana <wybson@hotmail.com>
2025-03-22 21:52:01 -0300

    JFoods: 11.14 - Adding and Using ModelMapper

--- src/main/java/br/dev/s2w/jfoods/api/adapter/assembler/RestaurantInputDisassembler.java
+++ src/main/java/br/dev/s2w/jfoods/api/adapter/assembler/RestaurantInputDisassembler.java
@@ -1,23 +1,19 @@
 package br.dev.s2w.jfoods.api.adapter.assembler;
 
 import br.dev.s2w.jfoods.api.adapter.model.input.RestaurantInput;
-import br.dev.s2w.jfoods.api.domain.model.Cuisine;
 import br.dev.s2w.jfoods.api.domain.model.Restaurant;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestaurantInputDisassembler {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public Restaurant toDomainObject(RestaurantInput restaurantInput) {
-        Cuisine cuisine = new Cuisine();
-        cuisine.setId(restaurantInput.getCuisine().getId());
-
-        Restaurant restaurant = new Restaurant();
-        restaurant.setName(restaurantInput.getName());
-        restaurant.setDeliveryFee(restaurantInput.getDeliveryFee());
-        restaurant.setCuisine(cuisine);
-
-        return restaurant;
+        return modelMapper.map(restaurantInput, Restaurant.class);
     }
 
 }
--- src/main/java/br/dev/s2w/jfoods/api/adapter/assembler/RestaurantModelAssembler.java
+++ src/main/java/br/dev/s2w/jfoods/api/adapter/assembler/RestaurantModelAssembler.java
@@ -1,8 +1,9 @@
 package br.dev.s2w.jfoods.api.adapter.assembler;
 
-import br.dev.s2w.jfoods.api.adapter.model.CuisineModel;
 import br.dev.s2w.jfoods.api.adapter.model.RestaurantModel;
 import br.dev.s2w.jfoods.api.domain.model.Restaurant;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,18 +12,11 @@ import java.util.stream.Collectors;
 @Component
 public class RestaurantModelAssembler {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public RestaurantModel toModel(Restaurant restaurant) {
-        CuisineModel cuisineModel = new CuisineModel();
-        cuisineModel.setId(restaurant.getCuisine().getId());
-        cuisineModel.setName(restaurant.getCuisine().getName());
-
-        RestaurantModel restaurantModel = new RestaurantModel();
-        restaurantModel.setId(restaurant.getId());
-        restaurantModel.setName(restaurant.getName());
-        restaurantModel.setDeliveryFee(restaurant.getDeliveryFee());
-        restaurantModel.setCuisine(cuisineModel);
-
-        return restaurantModel;
+        return modelMapper.map(restaurant, RestaurantModel.class);
     }
 
     public List<RestaurantModel> toCollectionModel(List<Restaurant> restaurants) {
